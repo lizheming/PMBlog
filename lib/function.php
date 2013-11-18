9ce14026ee8a7f5194c86c4624b2a3d269fd9f9d
怡红公子 <secretlzm007@gmail.com>
2013-11-18 20:31:21 +0800

    change removeDir()

--- lib/function.php
+++ lib/function.php
@@ -3,10 +3,8 @@ function mkfolder($path)	{
 	return is_writeable($path) || mkdir($path, 0777, true);
 }
 
-function paginator($post,$type,$name) {
+function paginator($post) {
 	global $site;
-	$prefix_url = $site['url'];
-	$prefix_url .= !empty($type) ? '/'.$type.'/'.$name.'/' : '/';
 	$perpage = $site['config']['posts_per_page'];
 	$total_pages = ceil(count($post)/$perpage);
 	for($i=0;$i<$total_pages;$i++) {
@@ -15,18 +13,17 @@ function paginator($post,$type,$name) {
 		if($i<1) $has_previous = false; 
 		$pagpost = array_slice($post, $i*$perpage, $perpage);
 		$paginator[$i+1] =  array(
-			'prefix_url' => $prefix_url,
 			'per_page' => $perpage,
 			'object_list'=>$pagpost, 
 			'total_pages'=> $total_pages, 
 			'page' => $i+1, 
-			'page_url' => $prefix_url.'page/'.($i+1),
+			'page_url' => $site['url'].'/page/'.($i+1),
 			'previous_page' => $i,
 			'pre_page' => $i, 
-			'previous_page_url' => $prefix_url.'page/'.$i,
-			'pre_page_url' => $prefix_url.'page/'.$i, 
+			'previous_page_url' => $site['url'].'/page/'.$i,
+			'pre_page_url' => $site['url'].'/page/'.$i, 
 			'next_page' => $i+2, 
-			'next_page_url' => $prefix_url.'page/'.($i+2), 
+			'next_page_url' => $site['url'].'/page/'.($i+2), 
 			'has_next' => $has_next, 
 			'has_previous' => $has_previous,
 			'has_pre' => $has_previous
@@ -36,10 +33,16 @@ function paginator($post,$type,$name) {
 }
 
 
-function DDir($dirName) {
-	foreach(glob($dirName.'/*') as $dir)	is_dir($dir) ? DDir($dir) && rmdir($dir) : unlink($dir);
+function removeDir($dirName) { 
+        foreach(glob($dirName.'/*') as $dir) {
+        		if(is_file($dir)) {
+        			@unlink($dir);
+        			continue;
+        		}
+                foreach(glob($dir.'/*') as $f) @unlink($f);
+                rmdir($dir);
+        }
 }
-
 
 function daily($dir) {
 	$doc = array();
