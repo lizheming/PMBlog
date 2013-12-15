cfdbad98edc70f9ec4919b5f876ed3cd4e5c3759
怡红公子 <secretlzm007@gmail.com>
2013-12-15 14:41:00 +0800

    增加后台登陆验证插件

---
+++ Authlogin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PMBlog plugin to add login for your panel
+ * 
+ * 
+ * @author lizheming
+ * @link http://github.com/lizheming
+ * @version 0.1.0
+ */
+
+class Authlogin {
+	/**
+	 * Change default username and password by yourself.
+	 */
+	private $username = "admin";
+	private $password = "admin";
+
+	public function plugins_loaded() {
+		/* Check user's name and password is default */
+		if($this->username == 'admin' && $this->password == 'admin') 
+			die('You have default username and password, modify them by yourself before run PMBlog.');
+		
+		$AUTHUSER = ( @$_SERVER['PHP_AUTH_USER'] == $this->username );
+		$AUTHPW = ( @$_SERVER['PHP_AUTH_PW'] == $this->password );
+		if(!$AUTHUSER || !$AUTHPW) {
+		    header('WWW-Authenticate: Basic realm="PMBlog Panel"');
+		    header('HTTP/1.1 401 Unauthorized');
+		    die();
+		}
+	}
+}
