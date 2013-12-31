e8491c152658d059a3a818ebf409bea5943253c7
怡红公子 <secretlzm007@gmail.com>
2013-12-31 15:16:09 +0800

    增加获取文章信息

--- Editor/Editor.php
+++ Editor/Editor.php
@@ -70,7 +70,8 @@ class Editor {
 		}
 	}
 
-	function get_hidden_post_meta(&$post) {
+	function get_hidden_post_meta(&$post) 
+	{
 		$hidden = array(
 			'type'=>$post->type(),
 			'docpath'=>$post->doc(),
@@ -83,13 +84,65 @@ class Editor {
 		
 		$this->hidden_posts[$hidden['type']][] = $hidden;
 	}
+
 	function DieInfo ( $value='')
 	{
 		$html = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body><div>{$value}</div></body></html>";
 		die($html);
 	}
+
 	function mkdir($path)
 	{
 		return is_writeable($path) || mkdir($path, 0777, true);
 	}
+
+    function get_content($doc) 
+    {
+        include ROOT_DIR."/var/parse.php";
+        $site = $this->config['config'];
+		$post = new parse($doc);
+		
+		$log['status'] = $post->status();
+        $date = $post->date();
+        $log['date'] = date($site['dateformat'], $date);
+        $log['type'] = $post->type();
+        $log['docpath'] = $post->doc();
+        $log['filename'] = $post->doc_title();
+        $log['extension'] = $post->doc_extension();
+        $log['title'] = $post->title();
+        $log['content'] = $post->text();
+        $log['template'] = $post->tmp();
+        $log['tags'] = $post->tags();
+        $log['custom'] = $post->custom();
+        if($log['type'] == 'post') {
+            $dirname = dirname($doc).'/';
+            $dirname = str_replace($site['doc'], '', $dirname);
+            $dirname = explode('/', $dirname);
+            $dirname = array_filter($dirname);
+            $log['categories'] = $post->categories();
+            if($log['categories'][0] != 'unclassified') {
+                $log['categories'] = array_merge($log['categories'], $dirname);
+            } else {
+                if(!empty($dirname))
+                $log['categories'] = $dirname;
+            }
+        }
+
+        $router = str_replace(
+            array('{year}', '{month}', '{day}'),
+            array(date('Y', $date), date('m', $date), date('d', $date)),
+            $site['router'][$log['type']]
+        );
+        if($log['type'] == 'post' && preg_match('/{category}/', $router)) {
+            $log['filepath'] = array();
+            foreach($log['categories'] as $category) {
+                $log['filepath'][] = str_replace('{category}', $category, $router) . "/{$log['filename']}.html";
+            }
+        } else {
+            $log['filepath'] = array("$router/{$log['filename']}.html");
+        }
+		$log['url'] = $this->config['url'].$log['filepath'][0];
+
+        return $log;
+    }
 }
