f289f25d5b822eb9ff9febf6795d5e8c702265db
perichr <i@perichr.org>
2014-03-17 14:07:58 +0800

    修复Editor插件bug
    修复删除、修复链接窗口背景、增加md预览样式

--- Editor/Editor.php
+++ Editor/Editor.php
@@ -3,7 +3,7 @@
  * A online editor plugin for PMBlog
  *
  * @author 公子
- * @version 0.0.1
+ * @version 0.1.1
  * @link http://github.com/lizheming
  */
 class Editor {
@@ -16,7 +16,6 @@ class Editor {
 
 	function config_loaded($config) {
 		$this->config = $config;
-
 		if(isset($_GET['edit'])) {
 			/* show editor */
 			$docpath = urldecode($_GET['edit']);
@@ -38,10 +37,10 @@ class Editor {
 		}
 
 		if(isset($_GET['rm'])) {
-			$file = ROOT_DIR.CONTENTS_DIR.'/'.urldecode($_GET['rm']);
-			if(!unlink($file)) die('删除出错请重新删除');
+			$file = urldecode($_GET['rm']);
+			
+			if(!unlink($file)) $this->die_info('删除出错，请重新删除或手动删除');
 		}
-
 
 		if(isset($_POST['docpath']) && isset($_POST['editortext'])) {
 			$newfile = $_POST['newfile'] == 'true';
@@ -51,13 +50,13 @@ class Editor {
 			$dirname = dirname($file);
 			if($newfile){
 				if(file_exists($file)){
-					$this->DieInfo("<script>alert(\'已经有同名文档 “".$name."” 了，请换个名字保存！\');window.location.href=\'?edit\';</script>");
+					$this->die_info("<script>alert(\'已经有同名文档 “".$name."” 了，请换个名字保存！\');window.location.href=\'?edit\';</script>");
 				}
 				$this->mkdir($dirname);
 			}
 			$res = file_put_contents($file, $_POST['editortext']);
 			echo "<script>localStorage.text='';</script>";
-			if(!$res) $this->DieInfo('文章保存失败');
+			if(!$res) $this->die_info('文章保存失败');
 		}
 	}
 
@@ -73,8 +72,7 @@ class Editor {
 		}
 	}
 
-	function get_hidden_post_meta(&$post) 
-	{
+	function get_hidden_post_meta(&$post) {
 		$hidden = array(
 			'type'=>$post->type(),
 			'docpath'=>$post->doc(),
@@ -87,65 +85,13 @@ class Editor {
 		
 		$this->hidden_posts[$hidden['type']][] = $hidden;
 	}
-
-	function DieInfo ( $value='')
+	function die_info ( $value='')
 	{
 		$html = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body><div>{$value}</div></body></html>";
 		die($html);
 	}
-
 	function mkdir($path)
 	{
 		return is_writeable($path) || mkdir($path, 0777, true);
 	}
-
-    function get_content($doc) 
-    {
-        include ROOT_DIR."/var/parse.php";
-        $site = $this->config['config'];
-		$post = new parse($doc);
-		
-		$log['status'] = $post->status();
-        $date = $post->date();
-        $log['date'] = date($site['dateformat'], $date);
-        $log['type'] = $post->type();
-        $log['docpath'] = $post->doc();
-        $log['filename'] = $post->doc_title();
-        $log['extension'] = $post->doc_extension();
-        $log['title'] = $post->title();
-        $log['content'] = $post->text();
-        $log['template'] = $post->tmp();
-        $log['tags'] = $post->tags();
-        $log['custom'] = $post->custom();
-        if($log['type'] == 'post') {
-            $dirname = dirname($doc).'/';
-            $dirname = str_replace($site['doc'], '', $dirname);
-            $dirname = explode('/', $dirname);
-            $dirname = array_filter($dirname);
-            $log['categories'] = $post->categories();
-            if($log['categories'][0] != 'unclassified') {
-                $log['categories'] = array_merge($log['categories'], $dirname);
-            } else {
-                if(!empty($dirname))
-                $log['categories'] = $dirname;
-            }
-        }
-
-        $router = str_replace(
-            array('{year}', '{month}', '{day}'),
-            array(date('Y', $date), date('m', $date), date('d', $date)),
-            $site['router'][$log['type']]
-        );
-        if($log['type'] == 'post' && preg_match('/{category}/', $router)) {
-            $log['filepath'] = array();
-            foreach($log['categories'] as $category) {
-                $log['filepath'][] = str_replace('{category}', $category, $router) . "/{$log['filename']}.html";
-            }
-        } else {
-            $log['filepath'] = array("$router/{$log['filename']}.html");
-        }
-		$log['url'] = $this->config['url'].$log['filepath'][0];
-
-        return $log;
-    }
 }
